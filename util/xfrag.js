376ac7856a8a1272928c041ad50cfe5adba7a97f
ArnaudBuchholz <arnaud.buchholz@free.fr>
2022-02-19 17:00:51 -0500

    xfrag

---
+++ util/xfrag.js
@@ -0,0 +1,15 @@
+sap.ui.define([
+  'sap/ui/core/Fragment'
+], function (Fragment) {
+  return function xfrag (xmlContent) {
+    return Fragment.load({
+      type: 'XML',
+      definition: `
+<core:FragmentDefinition
+  xmlns:core="sap.ui.core"
+>
+  ${xmlContent}
+</core:FragmentDefinition>`
+    });
+  }
+});

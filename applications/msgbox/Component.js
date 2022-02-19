c398657b6261380a19feaaa704e7aadeeace3136
ArnaudBuchholz <arnaud.buchholz@free.fr>
2022-02-19 17:00:12 -0500

    msgbox sample app

---
+++ applications/msgbox/Component.js
@@ -0,0 +1,9 @@
+sap.ui.define([
+  "sap/ui/core/UIComponent"
+], function(UIComponent) {
+  return UIComponent.extend("samples.applications.msgbox.Component", {
+    metadata: {
+      manifest: "json"
+    }
+  });
+});
---
+++ applications/msgbox/controller/Main.controller.js
@@ -0,0 +1,29 @@
+sap.ui.define([
+  'samples/util/BaseController',
+  'samples/util/messageBox',
+  'samples/util/asyncEventHandler',
+  'sap/m/MessageBox',
+], function (BaseController, messageBox, asyncEventHandler, MessageBox) {
+  return BaseController.extend('samples.applications.msgbox.controller.Main', {
+    onInfo: asyncEventHandler(async function () {
+      await messageBox(await this.i18n('btn.info.message'));
+    }),
+
+    onError: asyncEventHandler(async function () {
+      await messageBox(await this.i18n('btn.error.message'), {
+        icon: MessageBox.Icon.ERROR
+      });
+    }),
+
+    onQuestion: asyncEventHandler(async function () {
+      const answer = await messageBox(await this.i18n('btn.question.message'), {
+        icon: sap.m.MessageBox.Icon.QUESTION,
+        actions: [MessageBox.Action.YES, MessageBox.Action.NO],
+        emphasizedAction: MessageBox.Action.YES
+      });
+      await messageBox(answer || await this.i18n('btn.question.no-answer'), {
+        title: await this.i18n('btn.question.answer')
+      });
+    })
+  });
+});

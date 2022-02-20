6060930ce32a20fe1be4c3fdf240c47794605b76
ArnaudBuchholz <arnaud.buchholz@free.fr>
2022-02-20 09:24:50 -0500

    Improve logo patching

--- cdm/init.js
+++ cdm/init.js
@@ -1,19 +1,27 @@
 sap.ui.require([
 ], function () {
-  const shellHeader = document.getElementById('shell-header');
-  function setLogo () {
-    if (!setLogo.inProgress) {
-      setLogo.inProgress = true
-      const companyLogo = document.getElementById('shell-header-icon');
-      if (companyLogo) {
-        companyLogo.setAttribute('src', '/ui5_orange.svg');
-        companyLogo.setAttribute('width', '32px');
-      }
-      setTimeout(() => delete setLogo.inProgress, 0);
+  let shellHeaderObserver
+  function lazyLogoPatcher () {
+    if (shellHeaderObserver !== undefined) {
+      shellHeaderObserver.disconnect();
     }
+    const shellHeader = document.getElementById('shell-header');
+    function setLogo () {
+      if (!setLogo.inProgress) {
+        setLogo.inProgress = true
+        const companyLogo = document.getElementById('shell-header-icon');
+        if (companyLogo) {
+          companyLogo.setAttribute('src', 'ui5_orange.svg');
+          companyLogo.setAttribute('width', '32px');
+        }
+        setTimeout(() => delete setLogo.inProgress, 0);
+      }
+    }
+    shellHeaderObserver = new MutationObserver(setLogo);
+    shellHeaderObserver.observe(shellHeader, { attributes: true, childList: true, subtree: true });
   }
-  const shellHeaderObserver = new MutationObserver(setLogo);
-  shellHeaderObserver.observe(shellHeader, { attributes: true, childList: true, subtree: true });
+  lazyLogoPatcher();
+  sap.ui.getCore().attachThemeChanged(lazyLogoPatcher);
 
   if (window.location.search) {
     const sample = /\?(\w+)/.exec(window.location.search)[1];

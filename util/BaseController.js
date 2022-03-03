46c5b69fe602ff1c165c04f1415ade2e7323b429
ArnaudBuchholz <arnaud.buchholz@free.fr>
2022-03-03 01:15:33 -0500

    Simplify with class modifiers

--- util/BaseController.js
+++ util/BaseController.js
@@ -1,31 +1,21 @@
 sap.ui.define([
   'sap/ui/core/mvc/Controller',
-  'samples/util/i18n'
-], function (Controller, i18n) {
+  'samples/util/i18n',
+  'samples/util/classModifiers/eventBus',
+  'samples/util/classModifiers/readOnlyProperties',
+  'samples/util/classModifiers/defaultModel'
+], function (Controller, i18n, eventBus, readOnlyProperties, defaultModel) {
   const BaseController = Controller.extend('samples.util.BaseController', {
     ...i18n
   });
 
-  Object.defineProperties(BaseController.prototype, {
-    view: {
-      get: function () {
-        return this.getView();
-      }
-    },
-    defaultModel: {
-      get: function () {
-        return this.view.getModel();
-      },
-      set: function (model) {
-        this.view.setModel(model);
-      }
-    },
-    eventBus: {
-      get: function () {
-        return sap.ui.getCore().getEventBus();
-      }
+  eventBus(BaseController);
+  readOnlyProperties(BaseController, {
+    view: function () {
+      return this.getView();
     }
   });
+  defaultModel(BaseController, function () { return this.view; });
 
   return BaseController;
 });

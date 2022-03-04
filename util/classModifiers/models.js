b35d7705848ad440dc73cf3d85a789ba112af4a5
ArnaudBuchholz <arnaud.buchholz@free.fr>
2022-03-04 14:13:02 -0500

    Implement situation when the class is already a model provider

--- util/classModifiers/models.js
+++ util/classModifiers/models.js
@@ -3,17 +3,27 @@ sap.ui.define([
   'sap/ui/model/json/JSONModel'
 ], function (Model, JSONModel) {
   return function defaultModel (Class, modelsProvider) {
-    Object.assign(Class.prototype, {
-      getModel: function (name) {
-        return modelsProvider.call(this).getModel(name);
-      },
-      setModel: function (model, name) {
+    if (modelsProvider) {
+      Object.assign(Class.prototype, {
+        getModel: function (name) {
+          return modelsProvider.call(this).getModel(name);
+        },
+        setModel: function (model, name) {
+          if (!(model instanceof Model)) {
+            model = new JSONModel(model);
+          }
+          modelsProvider.call(this).setModel(model, name);
+        }
+      });
+    } else {
+      const inheritedSetModel = Class.prototype.setModel;
+      Class.prototype.setModel = function (model, name) {
         if (!(model instanceof Model)) {
           model = new JSONModel(model);
         }
-        modelsProvider.call(this).setModel(model, name);
-      }
-    });
+        inheritedSetModel.call(this, model, name);
+      };
+    }
     Object.defineProperty(Class.prototype, 'defaultModel', {
       get: function () {
         return this.getModel();

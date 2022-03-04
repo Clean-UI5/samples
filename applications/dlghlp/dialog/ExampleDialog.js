4424bde5111fbf05816d304300c5be5098d066c6
ArnaudBuchholz <arnaud.buchholz@free.fr>
2022-03-04 09:34:30 -0500

    Implements logic

--- applications/dlghlp/dialog/ExampleDialog.js
+++ applications/dlghlp/dialog/ExampleDialog.js
@@ -1,14 +1,30 @@
 sap.ui.define([
-  'samples/util/DialogController'
-], function (DialogController) {
+  'samples/util/DialogController',
+  'sap/m/MessageToast'
+], function (DialogController, MessageToast) {
   return DialogController.extend('samples.applications.dlghlp.dialog.ExampleDialog', {
+    onInit: async function () {
+      this.byId('switch').setTooltip(await this.i18n('dialog.switch.label'))
+      this.defaultModel = {};
+    },
+
+    onExit: async function () {
+      MessageToast.show(await this.i18n('dialog.destroyed'));
+    },
+
+    onBeforeOpen: function () {
+      this.defaultModel.setData({
+        state: false
+      });
+    },
+
     onOK: function () {
-      this.setResult(true);
+      this.setResult(this.defaultModel.getData());
       this.dialog.close();
     },
 
     onCancel: function () {
-      this.setResult(false);
+      this.setResult(null);
       this.dialog.close();
     }
   });

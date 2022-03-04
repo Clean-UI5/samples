17888d4de0f91397673275e850a7c48a569108b4
ArnaudBuchholz <arnaud.buchholz@free.fr>
2022-03-04 12:51:39 -0500

    Isolates code

---
+++ cdm/logoPatcher.js
@@ -0,0 +1,24 @@
+sap.ui.define([], function () {
+  let shellHeaderObserver
+  function lazyLogoPatcher () {
+    if (shellHeaderObserver !== undefined) {
+      shellHeaderObserver.disconnect();
+    }
+    const shellHeader = document.getElementById('shell-header');
+    function setLogo () {
+      if (!setLogo.inProgress) {
+        setLogo.inProgress = true
+        const companyLogo = document.getElementById('shell-header-icon');
+        if (companyLogo) {
+          companyLogo.setAttribute('src', 'ui5_orange.svg');
+          companyLogo.setAttribute('width', '32px');
+        }
+        setTimeout(() => delete setLogo.inProgress, 0);
+      }
+    }
+    shellHeaderObserver = new MutationObserver(setLogo);
+    shellHeaderObserver.observe(shellHeader, { attributes: true, childList: true, subtree: true });
+  }
+  lazyLogoPatcher();
+  sap.ui.getCore().attachThemeChanged(lazyLogoPatcher);
+});

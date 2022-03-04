b968730c07593cbe523816d639b644a5c55c0b30
ArnaudBuchholz <arnaud.buchholz@free.fr>
2022-03-04 09:34:17 -0500

    Allocate the dialog once, show result

--- applications/dlghlp/controller/Main.controller.js
+++ applications/dlghlp/controller/Main.controller.js
@@ -1,12 +1,19 @@
 sap.ui.define([
   'samples/util/BaseController',
   'samples/util/asyncEventHandler',
-  'samples/applications/dlghlp/dialog/ExampleDialog'
-], function (BaseController, asyncEventHandler, ExampleDialog) {
+  'samples/applications/dlghlp/dialog/ExampleDialog',
+  'sap/m/MessageToast'
+], function (BaseController, asyncEventHandler, ExampleDialog, MessageToast) {
   return BaseController.extend('samples.applications.dlghlp.controller.Main', {
+    onInit: function () {
+      this._dialog = new ExampleDialog({
+        view: this.view
+      });
+    },
+
     onShow: asyncEventHandler(async function () {
-      const dialog = new ExampleDialog(this.view);
-      const result = await dialog.open();
+      const result = await this._dialog.open();
+      MessageToast.show(`Dialog result is ${JSON.stringify(result)}`)
     })
   });
 });

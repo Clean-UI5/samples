31ea650487f0fcfe72e13b489f33dcaf8aab52f4
ArnaudBuchholz <arnaud.buchholz@free.fr>
2022-03-04 12:51:43 -0500

    Isolates code

---
+++ cdm/appShortcut.js
@@ -0,0 +1,13 @@
+sap.ui.define([], function () {
+  if (window.location.search) {
+    const sample = /\?(\w+)/.exec(window.location.search)[1];
+    history.replaceState('', 'Clean-UI5 samples', 'index.html')
+    sap.ushell.Container.getServiceAsync('CrossApplicationNavigation')
+      .then(service => service.toExternal({
+        target: {
+          semanticObject: 'sample',
+          action: sample
+        }
+      }));
+  }
+});

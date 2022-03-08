7d04c52f9b2723681bc41ed4623d06f04446b8eb
ArnaudBuchholz <arnaud.buchholz@free.fr>
2022-03-07 19:40:55 -0500

    Linter fix

--- cdm/forkMeOnGithub.js
+++ cdm/forkMeOnGithub.js
@@ -1,12 +1,14 @@
 sap.ui.define([], function () {
   const renderer = sap.ushell.Container.getRenderer('fiori2');
+  // eslint-disable-next-line sap-no-hardcoded-url
+  const baseUrl = 'https://github.com/Clean-UI5/samples';
   renderer.addUserAction({
     controlType: 'sap.m.Button',
     oControlProperties: {
       icon: 'sap-icon://source-code',
       text: 'Fork me on GitHub',
       press: () => {
-        sap.m.URLHelper.redirect('https://github.com/Clean-UI5/samples', true);
+        sap.m.URLHelper.redirect(baseUrl, true);
       }
     },
     bIsVisible: true,

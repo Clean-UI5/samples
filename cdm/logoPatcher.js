1f4070af569f45b5c36a4b977dc71f185b3ecafd
ArnaudBuchholz <arnaud.buchholz@free.fr>
2022-03-07 19:41:02 -0500

    Linter fix

--- cdm/logoPatcher.js
+++ cdm/logoPatcher.js
@@ -1,14 +1,18 @@
 sap.ui.define([], function () {
   let shellHeaderObserver;
+
+  // eslint-disable-next-line sap-no-dom-access
+  const byId = id => document.getElementById(id);
+
   function lazyLogoPatcher () {
     if (shellHeaderObserver !== undefined) {
       shellHeaderObserver.disconnect();
     }
-    const shellHeader = document.getElementById('shell-header');
+    const shellHeader = byId('shell-header');
     function setLogo () {
       if (!setLogo.inProgress) {
         setLogo.inProgress = true;
-        const companyLogo = document.getElementById('shell-header-icon');
+        const companyLogo = byId('shell-header-icon');
         if (companyLogo) {
           companyLogo.setAttribute('src', 'ui5_orange.svg');
           companyLogo.setAttribute('width', '32px');

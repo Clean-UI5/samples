9acbd9109b57b4cc8abbfe26fc0c7e40fbfa7b05
ArnaudBuchholz <arnaud.buchholz@free.fr>
2022-03-11 16:25:43 -0500

    Linter fix

--- cdm/appShortcut.js
+++ cdm/appShortcut.js
@@ -3,7 +3,7 @@ sap.ui.define([], function () {
     const sample = /\?(\w+)/.exec(window.location.search)[1];
     history.replaceState('', 'Clean-UI5 samples', 'index.html');
     sap.ushell.Container.getServiceAsync('CrossApplicationNavigation')
-      .then(service => service.toExternal({
+      .then((service) => service.toExternal({
         target: {
           semanticObject: 'sample',
           action: sample

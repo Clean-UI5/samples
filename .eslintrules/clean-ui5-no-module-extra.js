aaeb77dad4f042d225df0a8d924d3d2053484111
ArnaudBuchholz <arnaud.buchholz@free.fr>
2022-03-12 16:33:49 -0500

    Linter fix

--- .eslintrules/clean-ui5-no-module-extra.js
+++ .eslintrules/clean-ui5-no-module-extra.js
@@ -6,13 +6,13 @@ module.exports = function (context) {
       if (node.body.length > 1) {
         // Detects at least one `sap.ui.module` or `sap.ui.require`
         const isSAPUI5Module = node.body.some((child) => {
-          return child.type === 'ExpressionStatement'
-            && child.expression.type === 'CallExpression'
-            && child.expression.callee.type === 'MemberExpression'
-            && child.expression.callee.object.type === 'MemberExpression'
-            && child.expression.callee.object.object.name === 'sap'
-            && child.expression.callee.object.property.name === 'ui'
-            && ['define', 'require'].includes(child.expression.callee.property.name);
+          return child.type === 'ExpressionStatement' &&
+            child.expression.type === 'CallExpression' &&
+            child.expression.callee.type === 'MemberExpression' &&
+            child.expression.callee.object.type === 'MemberExpression' &&
+            child.expression.callee.object.object.name === 'sap' &&
+            child.expression.callee.object.property.name === 'ui' &&
+            ['define', 'require'].includes(child.expression.callee.property.name);
         });
         if (isSAPUI5Module) {
           context.report(node, 'A SAPUI5 module should not have code outside of sap.ui.define / sap.ui.require');

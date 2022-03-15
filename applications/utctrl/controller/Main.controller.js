5a6ad3edb26e043f1640950c719c9dd8a659d013
ArnaudBuchholz <arnaud.buchholz@free.fr>
2022-03-15 11:11:54 -0400

    ESLint fix

--- applications/utctrl/controller/Main.controller.js
+++ applications/utctrl/controller/Main.controller.js
@@ -2,28 +2,37 @@ sap.ui.define([
   'samples/util/BaseController'
 ], function (BaseController) {
   return BaseController.extend('samples.applications.utctrl.controller.Main', {
+    _adjustSIControls: function ({ specialInstructionsVisible, finalizeReviewEnabled, acknowledgeSIVisible }) {
+      this.byId('siContent').setVisible(specialInstructionsVisible);
+      this.byId('finalizeReview').setEnabled(finalizeReviewEnabled);
+      this.byId('ackSI').setVisible(acknowledgeSIVisible);
+    },
+
     onSalesOrderLoaded: function (salesOrder) {
       const { threshold } = this.getSpecialInstructions();
-      const finalizeReview = this.byId('finalizeReview');
-      const specialInstructions = this.byId('siContent');
-      const acknowledgeSI = this.byId('ackSI');
       const specialInstructionsAcknowledged = salesOrder.specialInstruction !== null;
       const specialInstructionsAckInfo = this.byId('siAckInfo');
       specialInstructionsAckInfo.setVisible(specialInstructionsAcknowledged);
       if (salesOrder.amount > threshold && salesOrder.customer.vip) {
         if (specialInstructionsAcknowledged) {
-          specialInstructions.setVisible(false);
-          finalizeReview.setEnabled(true);
-          acknowledgeSI.setVisible(false);
+          this._adjustSIControls({
+            specialInstructionsVisible: false,
+            finalizeReviewEnabled: true,
+            acknowledgeSIVisible: false
+          });
         } else {
-          specialInstructions.setVisible(true);
-          finalizeReview.setEnabled(false);
-          acknowledgeSI.setVisible(true);
+          this._adjustSIControls({
+            specialInstructionsVisible: true,
+            finalizeReviewEnabled: false,
+            acknowledgeSIVisible: true
+          });
         }
       } else {
-        specialInstructions.setVisible(false);
-        finalizeReview.setEnabled(true);
-        acknowledgeSI.setVisible(false);
+        this._adjustSIControls({
+          specialInstructionsVisible: false,
+          finalizeReviewEnabled: true,
+          acknowledgeSIVisible: false
+        });
       }
     }
   });

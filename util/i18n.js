7dc6e165ab2ec5233b6a7fc671cdd0c06dc45dc4
ArnaudBuchholz <arnaud.buchholz@free.fr>
2022-04-04 15:09:29 -0400

    Converts into method

--- util/i18n.js
+++ util/i18n.js
@@ -1,22 +1,20 @@
 sap.ui.define([
   'sap/base/Log'
 ], function (Log) {
-  return {
-    i18n: async function (key, ...args) {
-      let resourceBundle;
-      try {
-        resourceBundle = await this.getModel('i18n').getResourceBundle();
-        return resourceBundle.getText(key, args);
-      } catch (e) {
-        Log.error('An exception occurred while calling i18n', e.toString(), this.getMetadata().getName(), () => {
-          return {
-            resourceBundle,
-            key,
-            args
-          };
-        });
-        return key; // Best we can do
-      }
+  return async function i18n (key, ...args) {
+    let resourceBundle;
+    try {
+      resourceBundle = await this.getModel('i18n').getResourceBundle();
+      return resourceBundle.getText(key, args);
+    } catch (e) {
+      Log.error('An exception occurred while calling i18n', e.toString(), this.getMetadata().getName(), () => {
+        return {
+          resourceBundle,
+          key,
+          args
+        };
+      });
+      return key; // Best we can do
     }
   };
 });
